4b13c94bd64e809f3ca902091efab453dbbebe57
Andres <andres.enrique98@gmail.com>
2023-08-05 10:27:15 -0500

    initial project

---
+++ src/main/java/api/service/hackatonv1/HackatonV1Application.java
@@ -0,0 +1,13 @@
+package api.service.hackatonv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatonV1Application.class, args);
+	}
+
+}
