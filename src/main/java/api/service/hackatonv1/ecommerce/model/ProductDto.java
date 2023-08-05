913aac510b673192fde6e5b2313ba8012b2dd242
Andres <andres.enrique98@gmail.com>
2023-08-05 17:43:08 -0500

    database v2

--- src/main/java/api/service/hackatonv1/ecommerce/model/ProductDto.java
+++ src/main/java/api/service/hackatonv1/ecommerce/model/ProductDto.java
@@ -1,4 +1,9 @@
 package api.service.hackatonv1.ecommerce.model;
 
 public class ProductDto {
+    private String id;
+    private String name;
+    private double price;
+    private String description;
+    private Integer stock;
 }
